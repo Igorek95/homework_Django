326b9550c256032720d108cf67af37a796c95c17
Garik95 <dzerbuni@mail.ru>
2023-11-03 01:54:29 +0300

    Homework done

--- homework2/catalog/urls.py
+++ homework2/catalog/urls.py
@@ -1,6 +1,6 @@
-
 from django.urls import path
-from catalog.views import home, contacts, last_5_products, products, product, add_product
+from catalog.views import home, contacts, last_5_products, ProductCreateView, ProductListView, ProductDetailView, \
+    EntryCreateView, EntryListView, EntryDetailView, EntryDeleteView, EntryUpdateView
 
 app_name = 'catalog'
 
@@ -8,7 +8,12 @@ urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contact'),
     path('last-5-products/', last_5_products, name='last_5_products'),
-    path('products/', products, name='products'),
-    path('products/<int:pk>/', product, name='product'),
-    path('add_product/', add_product, name='add_product'),
+    path('products/', ProductListView.as_view(), name='products'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('add_product/', ProductCreateView.as_view(), name='add_product'),
+    path('blog/add_entry', EntryCreateView.as_view(), name='add_entry'),
+    path('blog/', EntryListView.as_view(), name='list_entry'),
+    path('blog/<int:pk>', EntryDetailView.as_view(), name='entry_details'),
+    path('blog/<int:pk>/delete', EntryDeleteView.as_view(), name='delete_entry'),
+    path('blog/<int:pk>/update', EntryUpdateView.as_view(), name='update_entry'),
 ]
